908e9bdb7b05ac5620a2ad432db14f5f0dc6611e
Terrance Rose Jr <trose618@gmail.com>
2018-10-26 13:19:20 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -178,9 +178,8 @@ end
 
 def player_stats(player_name)
   game_hash.find do |location, team_data|
-    if team_data[:player][player_name]
-      binding.pry
-      #team_data[:player][player_name]
+    if team_data[:players][player_name]
+      team_data[:players][player_name]
     end
   end
 end
